64618acd1582469fc1c5cce56db2ec2747cad9d0
CurtisXuCAD <CurtisXuCAD@users.noreply.github.com>
2021-10-02 17:17:10 -0700

    New database file uploaded

--- SqlFile/search.py
+++ SqlFile/search.py
@@ -3,7 +3,7 @@ with open("SqlFile/to_search.sql", "r") as f:
     results = ''
     set1 = set()
     for l in lines:
-        s = l[48:57]
+        s = l[36:45]
         if s not in set1:
             results += s + '|'
             print(s)
