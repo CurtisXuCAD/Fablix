8203a3a0aca05836934a1919fd51ee111374c12c
CurtisXuCAD <CurtisXuCAD@users.noreply.github.com>
2021-10-02 17:17:45 -0700

    New database file uploaded

--- SqlFile/search.py
+++ SqlFile/search.py
@@ -8,6 +8,6 @@ with open("SqlFile/to_search.sql", "r") as f:
             results += s + '|'
             print(s)
             set1.add(s)
-    with open("search_line.txt", "w") as out:
+    with open("SqlFile/search_line.txt", "w") as out:
         out.write(results[:-1])
         print(len(set1))
