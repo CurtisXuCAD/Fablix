f05a46ddaded7573f6d9a57d59e4dd1a0c0eb1aa
CurtisXuCAD <chunzhix@uci.edu>
2021-12-03 19:48:16 -0800

    add logparser

--- logParser.py
+++ logParser.py
@@ -7,8 +7,8 @@ tj = 0
 # Strips the newline character
 for line in Lines:
     count += 1
-    ts += line.split(",")[0]
-    tj += line.split(",")[1]
+    ts += int(line.split(",")[0])
+    tj += int(line.split(",")[1])
 
 ts_avg = ts / float(count) / 1000000.0
 tj_avg = tj / float(count) / 1000000.0
