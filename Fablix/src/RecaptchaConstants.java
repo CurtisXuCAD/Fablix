dca92ef3861feb1ea0236bcdc0969f1f92e77232
Chunzhi Xu <56098868+CurtisXuCAD@users.noreply.github.com>
2021-12-09 12:12:37 -0800

    Update RecaptchaConstants.java

--- Fablix/src/RecaptchaConstants.java
+++ Fablix/src/RecaptchaConstants.java
@@ -3,7 +3,6 @@ public class RecaptchaConstants {
 
     // TODO: Note that you should never hardcode your secret key, we are doing here for
     //  demonstration purpose only.
-    public static final String SECRET_KEY ="6LdZQRgdAAAAABFOoFKiegsFlq4EUSQGnZ-yV-X2";
-    //public static final String SECRET_KEY ="6LdZQRgdAAAAABFOoFKiegsFlq4EUSQGnZ-yV-X2";
+    public static final String SECRET_KEY ="";
 
 }
